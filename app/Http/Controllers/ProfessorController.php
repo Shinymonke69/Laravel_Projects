3726b22dccd3140f2da20d8ee4895c6c56baaba1
Shinymonke69 <gabrielbrazdacostag@gmail.com>
2025-08-21 19:49:42 -0300

    Feat:TrabalhoProfessor

---
+++ app/Http/Controllers/ProfessorController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProfessorController extends Controller
+{
+    public function index(){
+        $professores = Professor::all();
+        return view('professores.index', compact('professores'));
+    }
+
+    public function create(){
+        return view('professores.create');
+    }
+
+    public function store(Request $request){
+        Professor::create($request->all());
+        return redirect()->route('professores.index');
+    }
+
+    public function edit(Professor $professor){
+        return view('professores.edit', compact('professor'));
+    }
+
+    public function update(Request $request, Professor $professor){
+        $professor->update($request->all());
+        return redirect()->route('professores.index');
+    }
+
+    public function destroy(Professor $professor){
+        $professor->delete();
+        return redirect()->route('professores.index');
+    }
+}
